a3ec9cde85abcfb5dbc55ee20484642588818555
Nikolay <tng.attractor@gmail.com>
2023-07-19 21:24:09 +0600

    fixed sidebar html structure

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,21 +7,23 @@ interface Props {
 
 const Sidebar = ({ children }: Props) => {
   return (
-    <aside className="flex ">
-      <nav className="bg-base-100 w-64 flex flex-col h-max">
-        <ul className="flex flex-col py-4 px-6  menu ">
-          <li>
-            <NavLink to={"/quotes"} end>
-              All
-            </NavLink>
-            <NavLink to={"/quotes/star-wars"} end>
-              Star wars
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <aside className="basis-1/4">
+        <nav className="bg-base-100 h-max">
+          <ul className="flex flex-col py-4 px-6  menu ">
+            <li>
+              <NavLink to={"/quotes"} end>
+                All
+              </NavLink>
+              <NavLink to={"/quotes/star-wars"} end>
+                Star wars
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
       <div className="flex-grow">{children}</div>
-    </aside>
+    </>
   );
 };
 
